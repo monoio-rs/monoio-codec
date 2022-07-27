7877f1797ad32aa86fef42926f90f3ec1a77c778
ihciah <ihciah@gmail.com>
2022-07-27 15:38:03 +0800

    chore: add rustfmt.toml & do fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@
 mod async_codec;
 mod framed;
 
-pub use tokio_util::codec::Decoder;
-pub use tokio_util::codec::Encoder;
-
 pub use async_codec::{AsyncDecoder, AsyncEncoder};
 pub use framed::{Framed, FramedRead, FramedWrite};
+pub use tokio_util::codec::{Decoder, Encoder};
