0a16367dc9a9a96669c7a49fa8a6b7b3222cbce9
Rain Jiang <rain.jiang@bytedance.com>
2023-11-13 17:46:18 +0000

    add back the Encoder trait from tokio-util

--- src/sync_codec.rs
+++ src/sync_codec.rs
@@ -2,6 +2,7 @@ use std::io;
 
 use bytes::BytesMut;
 use monoio::io::{AsyncReadRent, AsyncWriteRent};
+pub use tokio_util::codec::Encoder;
 
 use crate::Framed;
 
@@ -126,23 +127,4 @@ pub trait Decoder {
     {
         Framed::new(io, self)
     }
-}
-
-pub trait Encoder<Item> {
-    /// The type of encoding errors.
-    ///
-    /// [`FramedWrite`] requires `Encoder`s errors to implement `From<io::Error>`
-    /// in the interest letting it return `Error`s directly.
-    ///
-    /// [`FramedWrite`]: crate::codec::FramedWrite
-    type Error: From<io::Error>;
-
-    /// Encodes a frame into the buffer provided.
-    ///
-    /// This method will encode `item` into the byte buffer provided by `dst`.
-    /// The `dst` provided is an internal buffer of the [`FramedWrite`] instance and
-    /// will be written out when possible.
-    ///
-    /// [`FramedWrite`]: crate::codec::FramedWrite
-    fn encode(&mut self, item: Item, dst: &mut BytesMut) -> Result<(), Self::Error>;
 }
