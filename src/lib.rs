160dba48786e3fbcc0506b62693074fcb55c2440
ihciah <ihciah@gmail.com>
2024-03-01 07:04:26 +0000

    feat: support SinkWithCodec

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,7 @@ pub mod length_delimited;
 mod sync_codec;
 
 pub use async_codec::{AsyncDecoder, AsyncEncoder};
-pub use framed::{Framed, FramedRead, FramedWrite, NextWithCodec};
+#[deprecated]
+pub use framed::StreamWithCodec as NextWithCodec;
+pub use framed::{Framed, FramedRead, FramedWrite, SinkWithCodec, StreamWithCodec};
 pub use sync_codec::{Decoded, Decoder, Encoder};
